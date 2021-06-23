388a638b989268275e5f66b9b7e9237d092093aa
neer17 <neerajsewanisrt8@gmail.com>
2021-06-23 23:11:36 +0530

    feat: added a signup/login form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 // import Head from 'next/head'
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { GetStaticProps } from 'next'
 
@@ -8,33 +9,42 @@ import { API_BASE_URL } from '../helpers/constants'
 import staticData from './../data/data.json'
 
 import styles from './index.module.scss'
+import router from 'next/router'
 
 export default function Home({ data }: any) {
-  return (
-    <div id={'index_page_root'} className={styles.home_root}>
-      <section className={styles.left_part}>
-        <div className={styles.detailsContainer}>
-          <div className={styles.imageContainer}>
-            <Image
-              src={'https://img.icons8.com/bubbles/2x/user-male.png'}
-              width={100}
-              height={100}
-            />
-          </div>
-          <div className={styles.details}>
-            <h2 className={styles.nameContainer}>Neeraj Sewani</h2>
-            <h2 className={styles.emailContainer}>test@example.com</h2>
-          </div>
-        </div>
+  const [email, setEmail]: [string, any] = useState('')
 
-        <h5 className={styles.logoutBtn}>Logout</h5>
-      </section>
-      <section className={styles.right_part}>
-        {data?.map((item: any) => (
-          <Card key={item.id} superheroData={item} />
-        ))}
-      </section>
-    </div>
+  useEffect(() => {
+    const userEmail = localStorage.getItem('email')
+    if (userEmail != null && userEmail.length > 0) setEmail(userEmail)
+    else router.replace('/login')
+  }, [])
+
+  return (
+    <>
+      {email.length == 0 ? (
+        <div>LOADER</div>
+      ) : (
+        <div id={'index_page_root'} className={styles.home_root}>
+          <section className={styles.left_part}>
+            <div className={styles.detailsContainer}>
+              <div className={styles.imageContainer}></div>
+              <div className={styles.details}>
+                <h2 className={styles.nameContainer}>Neeraj Sewani</h2>
+                <h2 className={styles.emailContainer}>test@example.com</h2>
+              </div>
+            </div>
+
+            <h5 className={styles.logoutBtn}>Logout</h5>
+          </section>
+          <section className={styles.right_part}>
+            {data?.map((item: any) => (
+              <Card key={item.id} superheroData={item} />
+            ))}
+          </section>
+        </div>
+      )}
+    </>
   )
 }
 
---
+++ pages/login.tsx
@@ -0,0 +1,76 @@
+import React, { ReactElement } from 'react'
+import { Formik } from 'formik'
+import router, { useRouter } from 'next/router'
+
+interface Props {}
+
+import styles from './styles/login.module.scss'
+
+export default function Login({}: Props): ReactElement {
+  return (
+    <div className={styles.root}>
+      <div className={styles.card}>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validate={(values) => {
+            const errors = {}
+            if (!values.email) {
+              errors.email = 'Required'
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+              errors.email = 'Invalid email address'
+            }
+            return errors
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2))
+              setSubmitting(false)
+              localStorage.setItem('email', values.email)
+              router.push('/')
+            }, 400)
+          }}>
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            /* and other goodies */
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <div className={styles.inputWrapper}>
+                <label htmlFor="email-input">Email</label>
+                <input
+                  id="email-input"
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                />
+                {errors.email && touched.email && errors.email}
+              </div>
+              <div className={styles.inputWrapper}>
+                <label htmlFor="pass-input">Password</label>
+                <input
+                  id="pass-input"
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                />
+                {errors.password && touched.password && errors.password}
+              </div>
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  )
+}
