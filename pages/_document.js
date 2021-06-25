6ed991c1a98e39ecdfd47cb85f1c37810feb9b34
neer17 <neerajsewanisrt8@gmail.com>
2021-06-25 22:56:20 +0530

    test husky

--- pages/_document.js
+++ pages/_document.js
@@ -10,9 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <style>
-            
-          </style>
+          <style></style>
         </Head>
         <body>
           <Main />
